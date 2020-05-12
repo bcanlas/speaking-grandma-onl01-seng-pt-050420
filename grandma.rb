ddd5a263e785a2a70c2c1d292a735c7a00536782
Bianca Canlas <bcanlas87@gmail.com>
2020-05-12 02:28:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,4 +14,7 @@
 
 
 def speak_to_grandma(phrase)
-  if phrase == ""
+  if phrase == "I LOVE YOU GRANDMA!"
+    return = "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase != phrase.upcase
+  return "HUH?! SPEAK UP, SONNY!"
